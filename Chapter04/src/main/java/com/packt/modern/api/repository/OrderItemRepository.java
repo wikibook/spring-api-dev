6aec5196054e124c63863943bdece89b83f7e1d6
wikibook <dylee@wikibook.co.kr>
2024-11-28 16:46:36 +0900

    예제 코드

---
+++ Chapter04/src/main/java/com/packt/modern/api/repository/OrderItemRepository.java
@@ -0,0 +1,12 @@
+package com.packt.modern.api.repository;
+
+import com.packt.modern.api.entity.OrderItemEntity;
+import java.util.UUID;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author : github.com/sharmasourabh
+ * @project : Chapter04 - Modern API Development with Spring and Spring Boot Ed 2
+ **/
+public interface OrderItemRepository extends CrudRepository<OrderItemEntity, UUID> {
+}
---
+++ Chapter06/src/test/java/com/packt/modern/api/eCommerceAppTests.java
@@ -0,0 +1,13 @@
+package com.packt.modern.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class eCommerceAppTests {}
